ebcb8ca5f40a911241a940871cf30a046124cc74
camiloibanez <camiloibanez8@gmail.com>
2020-08-23 13:58:41 -0500

    new variables

--- PyBank/main.py
+++ PyBank/main.py
@@ -8,6 +8,9 @@ def average(numbers):
     average = sum(numbers)/len(numbers)
     return average
 
+def to_currency(value):
+    return '${:,}'.format(value)
+
 with open(file_path, 'r') as csvfile:
     budget_data_reader = csv.reader(csvfile, delimiter=',')
 
@@ -21,30 +24,39 @@ with open(file_path, 'r') as csvfile:
     previous_profit_loss = 0
 
     for row in budget_data_reader:
+        # Adding another month to the list of total months counted
         total_months += 1
+        # Adding to the total net profit loss
         net_total_profit_loss += int(row[1])
+        # Appending the date of the record
         months.append(row[0])
+        # Calculating the change in profit loss from the previous month to the next month
         next_profit_loss = int(row[1])
 
         change_in_profit_loss.append(next_profit_loss - previous_profit_loss)
 
         previous_profit_loss = int(row[1])
-
+    # Removing the first profit/loss because we don't have a previous month
     change_in_profit_loss.pop(0)
     average_change = average(change_in_profit_loss)
 
     max_profit_change = max(change_in_profit_loss)
     min_loss_change = min(change_in_profit_loss)
 
-    max_profit_month = months[change_in_profit_loss.index(max_profit_change) + 1]
-    min_loss_month = months[change_in_profit_loss.index(min_loss_change) + 1]
+    max_profit_change_index = change_in_profit_loss.index(max_profit_change) + 1
+    min_loss_change_index = change_in_profit_loss.index(min_loss_change) + 1
+
+    max_profit_month = months[max_profit_change_index]
+    min_loss_month = months[min_loss_change_index]
+
+    net_total_profit_loss_currency = to_currency(net_total_profit_loss)
 
     
 
 print("Financial Analysis")
 print("----------------------------")
 print(f"Total Months: {total_months}")
-print(f"Total: ${net_total_profit_loss}")
+print(f"Total: {net_total_profit_loss_currency}")
 print("Average Change: $%.2f" % average_change)
 print(f"Greatest Increase in Profits: {max_profit_month} (${max_profit_change})")
 print(f"Greatest Decrease in Profits: {min_loss_month} (${min_loss_change})")
@@ -55,7 +67,7 @@ with open(output_path, 'w') as txtfile:
     txtfile.write("Financial Analysis\n")
     txtfile.write("----------------------------\n")
     txtfile.write(f"Total Months: {total_months}\n")
-    txtfile.write(f"Total: ${net_total_profit_loss}\n")
+    txtfile.write(f"Total: {net_total_profit_loss_currency}\n")
     txtfile.write(f"Average Change: $%.2f" % average_change + "\n")
     txtfile.write(f"Greatest Increase in Profits: {max_profit_month} (${max_profit_change})\n")
     txtfile.write(f"Greatest Decrease in Profits: {min_loss_month} (${min_loss_change})")
