da4f1a4e74fc5abf1c944f87803e82216f129596
camiloibanez <camiloibanez8@gmail.com>
2020-08-25 18:59:09 -0500

    average word length

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -1,7 +1,9 @@
 import os
 import re
+import numpy as np
 
 file_path1 = os.path.join("raw_data", "paragraph_1.txt")
+
 
 def paragraphSummary(file_path):
     paragraph = open(file_path, "r")
@@ -12,8 +14,16 @@ def paragraphSummary(file_path):
     word_count = len(words)
 
     sentences = re.split("(?<=[.!?]) +", paragraph)
-
     sentence_count = len(sentences)
-    print(sentence_count)
+
+    word_lengths = []
+
+    for i in np.arange(0, word_count):
+        word_length = len(words[i])
+        word_lengths.append(word_length)
     
+    avg_letter_count = round(np.mean(word_lengths), 1)
+
+    print(avg_letter_count)
+
 paragraphSummary(file_path1)
